0672ac0873dd1c04d5890f6a20421620e837a0a1
Dániel Mozsár <daniel.mozsar01@gmail.com>
2024-09-29 23:35:53 +0200

    Fixed login button state

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -31,7 +31,6 @@ export const Login = () => {
 
   const onVerifyCaptcha = (token: any) => {
     if (token) {
-      setCaptchaState(true);
       setLoginAttempCount(0);
     }
   };
@@ -113,7 +112,7 @@ export const Login = () => {
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-3 disabled:bg-indigo-200"
-              disabled={(!isValid || !captchaState) && loginAttempCount >= 3}
+              disabled={!isValid || loginAttempCount >= 3}
             >
               Bejelentkezés
             </button>
