0987a2f16f86a31cb9a718ceb1c154f8c286ee46
Dániel Mozsár <daniel.mozsar01@gmail.com>
2024-09-30 19:48:59 +0200

    Added a bit of space next to authorities

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,26 +1,28 @@
-import { useAuth } from "../context/AuthContext"
+import { useAuth } from "../context/AuthContext";
 
 export const Home = () => {
-  const {authorities, username, lastLogin} = useAuth();
+  const { authorities, username, lastLogin } = useAuth();
 
   return (
-    <div className='flex items-center flex-col h-full'>
+    <div className="flex items-center flex-col h-full">
       <div className="flex">
-      <h2 className='font-bold mr-2'>Felhasználó név:</h2>
-      <span>{username}</span>
+        <h2 className="font-bold mr-2">Felhasználó név:</h2>
+        <span>{username}</span>
       </div>
       <div className="flex">
-      <h2 className='font-bold mr-2'>Szerepkör(ök): </h2>
-      <span>
-      {authorities.map((authority, index) => (
-        <span key={index}>{authority}</span>
-      ))}
-      </span>
+        <h2 className="font-bold mr-2">Szerepkör(ök): </h2>
+        <span>
+          {authorities.map((authority, index) => (
+            <span key={index}>{authority}, </span>
+          ))}
+        </span>
       </div>
       <div className="flex">
-      <h2 className='font-bold mr-2'>Utolsó bejelentkezés időpontja:</h2>
-      <span>{lastLogin ? lastLogin : 'Nincs rögzített bejelentkezési időpont'}</span>
+        <h2 className="font-bold mr-2">Utolsó bejelentkezés időpontja:</h2>
+        <span>
+          {lastLogin ? lastLogin : "Nincs rögzített bejelentkezési időpont"}
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
